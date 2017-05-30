7f0fdfec2b44b268726dcc66576774a20b1371ef
Thomas Marquart <thomas@marquart.se>
2017-05-30 21:21:02 +0200

    fix bug when no abs Entry exists for Stuff

--- keepstuff/views.py
+++ keepstuff/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView, DetailView
+from datetime import datetime
 from .models import *
 
 class CurrentStuff(ListView):
@@ -16,13 +17,16 @@ def recalc(request):
     update the stock value.
     """
     for stuff in Stuff.objects.all():
-        last_abs = Entry.objects.filter(stuff=stuff,isabs=True).order_by('-time')[0]
+        last_abs = Entry.objects.filter(stuff=stuff,isabs=True)
         if last_abs:
+            last_abs = last_abs.order_by('-time')[0]
             stuff.quant = last_abs.quant
+            time = last_abs.time
         else:
             stuff.quant = 0
+            time = datetime.min
 
-        for inc in Entry.objects.filter(stuff=stuff,isabs=False,time__gt=last_abs.time):
+        for inc in Entry.objects.filter(stuff=stuff,isabs=False,time__gt=time):
             stuff.quant += inc.quant
         stuff.save()
 
