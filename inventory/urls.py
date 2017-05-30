e8ebe0a5f16ccbb4f3ba687601fe0b800b445fa2
Thomas Marquart <thomas@marquart.se>
2017-05-30 15:12:24 +0200

    templates, model tweaks, start entering test data

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^inv/', include('keepstuff.urls')),
+    url(r'', include('keepstuff.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
--- keepstuff/admin.py
+++ keepstuff/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from .models import *
 admin.site.register(Category)
 admin.site.register(Stuff)
+admin.site.register(Entry)
