c5e708a8740cdf01ee75f31957ee0ca1d4358665
Thomas Marquart <thomas@marquart.se>
2017-05-30 13:50:54 +0200

    admin and more models

--- keepstuff/models.py
+++ keepstuff/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+class Category(Model):
+    name = CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+
+class Stuff(Model):
+    name = CharField(max_length=200)
+    unit = CharField(max_length=10)
+    quant = IntegerField(default=0)
+    catg = ForeignKey(Category)
+    lastmod = DateTimeField('last modified', auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Stock(Model):
+    stuff = ForeignKey(Stuff)
+    quant = IntegerField(default=0)
+    time = DateTimeField('entry timestamp', auto_now_add=True)
+
+class Diff(Model):
+    stuff = ForeignKey(Stuff)
+    quant = IntegerField(default=0)
+    time = DateTimeField('entry timestamp', auto_now_add=True)
